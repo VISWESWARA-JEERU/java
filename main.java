4f700b181cde382833f4f5200e05ac42e3750586
VISWESWARA-JEERU <jvsks28@gmail.com>
2024-07-26 16:12:20 +0530

    new folder

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        float a = 35.678f;
+        System.out.println("the value of a is "+ a);
+        double b =34.818181818;
+        System.out.println("the value of b is "+ b);
+    }
+}
